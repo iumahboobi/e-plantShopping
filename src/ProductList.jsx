dd4ed5c019f24f1231e23c53e3ce13003876fb1d
Ihsanullah Mahboobi <iumahboobi@gmail.com>
2025-03-02 15:05:49 +0100

    Categories header styled

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -361,9 +361,7 @@ function ProductList() {
         <div className="product-grid">
           {plantsArray.map((category, index) => (
             <div key={index}>
-              <h1>
-                <div>{category.category}</div>
-              </h1>
+              <h1><div className='category' >{category.category}</div></h1>
               <div className="product-list">
                 {category.plants.map((plant, plantIndex) => (
                   <div className="product-card" key={plantIndex}>
